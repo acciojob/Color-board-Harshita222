e24fb20c474d94880a2aff5b6d2b752ce36f6131
Harshita222 <124767756+Harshita222@users.noreply.github.com>
2024-05-08 17:16:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function() {
+  const board = document.getElementById('board');
+
+  // Create 800 boxes
+  for (let i = 0; i < 800; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    board.appendChild(box);
+  }
+});
